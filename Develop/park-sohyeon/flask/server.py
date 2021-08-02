762fb3156e8740f36092718c1e31c7863f435e10
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-08-03 01:05:31 +0900

    Update server.py

--- Develop/park-sohyeon/flask/server.py
+++ Develop/park-sohyeon/flask/server.py
@@ -23,16 +23,6 @@ def create_app(test_config = None):
     app.database = database
 
     return app
-
-
-# class Flight(db.Model):
-#     __tablename__ = "flights"
-#     id = db.Column(db.Integer, primary_key=True)
-#     origin = db.Column(db.String, nullable=False)
-#     destination = db.Column(db.String, nullable=False)    
-#     duration = db.Column(db.Integer, nullable=False)    
-
-
 @app.route('/', methods = ['GET'])
 def home():
     return "home"
@@ -42,62 +32,50 @@ def add():
     create_app()
 
     user = request.json
-    user_info = app.database.execute(text("""
-                                        INSERT INTO member (
-                                        member_id,
-                                        member_password,
-                                        member_name,
-                                        score
-                                       ) VALUES (
-                                        :member_id,
-                                        :member_password,
-                                        :score
-                                       )
-                                        """), user).lastrowid
+    app.database.execute(text("""
+                                INSERT INTO member (
+                                member_id,
+                                member_password,
+                                member_name,
+                                score
+                                ) VALUES (
+                                :member_id,
+                                :member_password,
+                                :score
+                                )
+                                """), user).lastrowid
 
     return "success", 200
 
 @app.route('/diary', methods = ['POST','GET'])
 def diary():
-    create_app()
-    created_at="hi"
-    user_diary = app.database.execute(text("""
-                                        INSERT INTO diary (created_at,member_id,text)
-                                        VALUES (created_at,member_id,text)
-                                    """), request.json).lastrowid
-    row = app.database.execute(text("""			# 1)
+    app = create_app()
+    userdiary = request.json
+    print((userdiary))
+    new_diary = app.database.execute(text("""
+                        INSERT INTO diary (
+                            created_at,
+                            member_id,
+                            text
+                        )""" + """VALUES (
+                            :created_at,
+                            :member_id,
+                            :text
+                        )
+                        """),userdiary).lastrowid
+                                    
+    row = app.database.execute(text("""
         SELECT
             created_at
         FROM test.diary
-    """), {
-        	'created_at' : created_at
-        }).fetchall()
+    """)
+    ).fetchall()
         
-    timeline = [{							# 2)
+    timeline = [{
         'created_at' : rows['created_at']
     } for rows in row]
-    
-    return jsonify({						# 3)
-        'created_at' : created_at
-    })
+    return jsonify(timeline[0])
 
-
-
-
-
-
-# conn = pymysql.connect()
-# temp = list()
-# for row in conn.execute('SELECT * FROM test.member'):
-#         temp.append(row)
-# for row in conn.execute('SELECT * FROM test.diary'):
-#         temp.append(row)
-# df = pd.DataFrame(temp)
-
-
-# session.query(member).\
-# outerjoin(diary, member.member_id == diary.member_id).__module__
-# all()
 
 if __name__== '__main__': # 모델로드
     app.run(host='192.168.0.104', port='8080', debug = True)
