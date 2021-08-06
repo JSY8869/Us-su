0c0011727d427914c3d7b766407fef1829b4fb5d
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-08-06 23:34:38 +0900

    Diary CRUD 초안 완성
    UI와 연결 예정입니다.

--- Develop/park-sohyeon/flask/server.py
+++ Develop/park-sohyeon/flask/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from sqlalchemy import create_engine, text
 from diary import Diary
-from member import User
+import pymysql
 
 app = Flask(__name__)
 username="root"
@@ -17,6 +17,17 @@ def create_app(test_config = None):
 
     return app
 
+# class User(db.Moedl):
+#     """Create user table"""
+#     id = app.Column(app.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique = True)
+#     password = db.Column(db.String(80))
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+
+
+
 @app.route('/', methods = ['GET'])
 def home():
     return "home"
@@ -30,7 +41,6 @@ def add():
                                 INSERT INTO member (
                                 member_id,
                                 member_password,
-                                member_name,
                                 score
                                 ) VALUES (
                                 :member_id,
@@ -48,7 +58,8 @@ def diary_R():
 
 @app.route('/diary/Create', methods = ['GET'])
 def diary_C():
-    return Diary.create(create_app())
+    result = create_app()
+    return Diary.create(result)
 
 @app.route('/diary/Update', methods = ['GET'])
 def diary_U():
@@ -59,9 +70,49 @@ def diary_U():
 def diary_D():
     return Diary.delete(create_app())
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return User.login(create_app())
+import re
+from sys import meta_path
+from types import MethodDescriptorType
+from flask import Flask, request, session, render_template, redirect, url_for
+import pymysql
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/login_form')
+def login_form():
+    return render_template('login_form.html')
+
+@app.route('/login_proc', methods=['POST'])
+def login_proc():
+    if request.method == 'POST':
+        member_id = request.form['id']
+        userPassword = request.form['pwd']
+        if len(member_id) == 0 or len(userPassword) == 0:
+            return 'not found!!'
+        else:
+            rows = create_app().database.execute(text("""
+                select member_pk, member_id, member_password from member where member_id = member_id
+            """),member_id).fetchall()
+            print (rows)
+            for rs in rows:
+                if member_id == rs[1] and userPassword == rs[2]:
+                    session['logFlag'] = True
+                    session['idx'] = rs[0]
+                    session['member_id'] = member_id
+                    return redirect(url_for('main'))
+                else:
+                    return redirect(url_for('login_form'))
+    else:
+        return '잘 못 박 소 현'
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('main'))
 
 if __name__== '__main__': # 모델로드
-    app.run(host='192.168.0.114', port='8080', debug = True)
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(host='192.168.0.113', port='8080', debug = True)
