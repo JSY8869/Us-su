bc8ad33b91ef29f8445294b30627b0ddfc84c24a
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-10-08 01:05:26 +0900

    Update dbconfig.py

--- Develop/park-sohyeon/flask/dbconfig.py
+++ Develop/park-sohyeon/flask/dbconfig.py
@@ -1,14 +1,2 @@
 import pymysql
 db= pymysql.connect(host='localhost',port=3306,user='root',db='mysql',charset='utf8')
-
-
-db.cursor().execute("show tables")
-sql = """INSERT INTO test_table(name)
-         VALUES('test_name')"""
- 
-db.cursor().execute(sql)
-db.commit()
-
-sql= """SELECT name FROM test_table"""
-
-db.close()
