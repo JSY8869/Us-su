22e06445b720b2ea7971bc91ef457c49fcdaa618
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-10-07 23:15:51 +0900

    Update dbconfig.py

--- Develop/park-sohyeon/flask/dbconfig.py
+++ Develop/park-sohyeon/flask/dbconfig.py
@@ -1,5 +1,5 @@
 import pymysql
-db= pymysql.connect(host='admin',
+db= pymysql.connect(host='us_su',
                      port=3306,
                      user='root',
                      passwd='0cN1KvzzVeQuO7xc7fmt',
