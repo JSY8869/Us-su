0d7d394eaab9f1afcb23c6ef371aa02f1bdeee3c
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-10-08 00:01:16 +0900

    Update dbconfig.py

--- Develop/park-sohyeon/flask/dbconfig.py
+++ Develop/park-sohyeon/flask/dbconfig.py
@@ -1,5 +1,5 @@
 import pymysql
-db= pymysql.connect(host='localhost',port=3306,user='root',passwd='0cN1KvzzVeQuO7xc7fmt',db='test',charset='utf8')
+db= pymysql.connect(host='localhost',port=3306,user='root',db='test',charset='utf8')
 
 
 db.cursor().execute("show tables")
