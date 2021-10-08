3565c16fae6827722d0e6a4c530a8df6f34b7e65
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-10-08 22:23:20 +0900

    예외처리 수정

--- Develop/park-sohyeon/flask/server_src/game.py
+++ Develop/park-sohyeon/flask/server_src/game.py
@@ -3,105 +3,87 @@ from sqlalchemy import text
 from game_src import NLP_game
 from game_src import NLP_game2
 def game_1(app): # 게임 1 실행 코드
-        try:
-                row = app.database.execute(text("""
-                                        SELECT text, score_ox1 FROM diary 
-                                        WHERE member_id= :member_id and created_at= :created_at
-                                        """),request.json).fetchone()
-                question, answer = NLP_game.NLP1.make_qa(row[0])
-                score = Read_Game_Score(app)
-                if score != -1:
-                        return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':question, 'answer':answer, 'score':score, 'score_ox1':row[1]})   
-                else:
-                        return 'error'
-        except:
+        row = app.database.execute(text("""
+                                SELECT text, score_ox1 FROM sys.diary 
+                                WHERE member_id= :member_id and created_at= :created_at
+                                """),request.json).fetchone()
+        question, answer = NLP_game.NLP1.make_qa(row[0])
+        score = Read_Game_Score(app)
+        if score != -1:
+                return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':question, 'answer':answer, 'score':score, 'score_ox1':row[1]})   
+        else:
                 return 'error'
 
 def game_2(app): # 게임 2 실행 코드
         row = []
         row = app.database.execute(text("""
-                                SELECT text, score_ox2 FROM diary 
+                                SELECT text, score_ox2 FROM sys.diary 
                                 WHERE member_id= :member_id and created_at= :created_at
                                 """),request.json).fetchall()
         
         if row != []:
                 if row[0][1] == 1: # 게임을 이미 진행한 경우
                         return 'error'
-                try:
-                        important_words = NLP_game2.NLP2.make_important_word(row[0][0])
-                        if important_words == None:
-                                return 'error'
-                        game_text = {}
-                        game_text['game_text'] = important_words
-                        SQL = 'SELECT game_text FROM game WHERE game_text in ("'
-                        SQL += '","'.join(game_text['game_text'])
-                        SQL += '")'
-                        row = app.database.execute(SQL).fetchall()
-                        row_string = ""
-                        for i in range(len(row)):
-                                row_string += ''.join(row[i])+' '
-                        if row_string != "":
-                                return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':row_string, 'score':Read_Game_Score(app), 'score_ox2':0} )
-                        else:
-                                important_word = ""
-                                for i in range(len(important_words)):
-                                        important_word += ''.join(important_words[i])+' '
-                                return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':99, 'game_text':important_word, 'score':Read_Game_Score(app), 'score_ox2':0})
-                except:
+                important_words = NLP_game2.NLP2.make_important_word(row[0][0])
+                if important_words == None:
                         return 'error'
+                game_text = {}
+                game_text['game_text'] = important_words
+                SQL = 'SELECT game_text FROM sys.game WHERE game_text in ("'
+                SQL += '","'.join(game_text['game_text'])
+                SQL += '")'
+                row = app.database.execute(SQL).fetchall()
+                row_string = ""
+                for i in range(len(row)):
+                        row_string += ''.join(row[i])+' '
+                if row_string != "":
+                        return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':row_string, 'score':Read_Game_Score(app), 'score_ox2':0} )
+                else:
+                        important_word = ""
+                        for i in range(len(important_words)):
+                                important_word += ''.join(important_words[i])+' '
+                        return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':99, 'game_text':important_word, 'score':Read_Game_Score(app), 'score_ox2':0})
         else:
                 return 'error'
 
 def plus_word(app): # DB에 '장소'관련 단어 추가
-        try:
-                game_text = {}
-                game_text['game_text'] = request.json['game_text'].split()
-                SQL = 'INSERT INTO game (game_text) VALUES ("'
-                SQL += '"),("'.join(game_text['game_text'])
-                SQL += '")'
-                app.database.execute(SQL).lastrowid
-                return json.dumps({'game_text':'99'})
-        except:
-                return 'error'
+        game_text = {}
+        game_text['game_text'] = request.json['game_text'].split()
+        SQL = 'INSERT INTO sys.game (game_text) VALUES ("'
+        SQL += '"),("'.join(game_text['game_text'])
+        SQL += '")'
+        app.database.execute(SQL).lastrowid
+        return json.dumps({'game_text':'99'})
                 
 def Update_Game_Score(app): # 게임 1 완료 정보 저장
-        try:
-                app.database.execute(text("""
-                                Update member
-                                SET score= :score
-                                Where member_id = :member_id
-                                """),request.json).lastrowid
-                app.database.execute(text("""
-                                Update diary
-                                SET score_ox1= 1
-                                Where member_id = :member_id and created_at = :created_at
-                                """),request.json).lastrowid
-                return (json.dumps({'score':request.json['score']}))
-        except:
-                return 'error'
+        app.database.execute(text("""
+                        Update sys.member
+                        SET score= :score
+                        Where member_id = :member_id
+                        """),request.json).lastrowid
+        app.database.execute(text("""
+                        Update sys.diary
+                        SET score_ox1= 1
+                        Where member_id = :member_id and created_at = :created_at
+                        """),request.json).lastrowid
+        return (json.dumps({'score':request.json['score']}))
 
 def Update_Game_Score2(app): # 게임 2 완료 정보 저장
-        try:
-                app.database.execute(text("""
-                                Update member
-                                SET score= :score
-                                Where member_id = :member_id
-                                """),request.json).lastrowid
-                app.database.execute(text("""
-                                Update diary
-                                SET score_ox2= 1
-                                Where member_id = :member_id and created_at = :created_at
-                                """),request.json).lastrowid
-                return (json.dumps({'score':request.json['score']}))
-        except:
-                return 'error'
+        app.database.execute(text("""
+                        Update sys.member
+                        SET score= :score
+                        Where member_id = :member_id
+                        """),request.json).lastrowid
+        app.database.execute(text("""
+                        Update sys.diary
+                        SET score_ox2= 1
+                        Where member_id = :member_id and created_at = :created_at
+                        """),request.json).lastrowid
+        return (json.dumps({'score':request.json['score']}))
 
 def Read_Game_Score(app): # 사용자의 score 현황 불러오기
-        try:
-                row = app.database.execute(text("""
-                                select score from member 
-                                where member_id = :member_id
-                                """),request.json).fetchone()
-                return row[0]
-        except:
-                return -1
+        row = app.database.execute(text("""
+                        select score from sys.member 
+                        where member_id = :member_id
+                        """),request.json).fetchone()
+        return row[0]
--- Develop/park-sohyeon/flask/server_src/member.py
+++ Develop/park-sohyeon/flask/server_src/member.py
@@ -10,17 +10,14 @@ class Member():
         return json.dumps({'member_id':row[0], 'member_password':row[1]})
 
     def register(app): # 회원 가입
-        try:
-            member_id_cor = app.database.execute(text("""
-                            select member_id from member 
-                            where member_id = :member_id
-                            """), request.json).fetchone()
-            if (member_id_cor != None): return "error"
-            else:
-                app.database.execute(text("""
-                                    INSERT INTO member (member_id, member_password, score)
-                                    VALUES (:member_id, :member_password, :score)
-                                    """), request.json).lastrowid
-                return request.json
-        except:
-            return 'error'
+        member_id_cor = app.database.execute(text("""
+                        select member_id from sys.member 
+                        where member_id = :member_id
+                        """), request.json).fetchone()
+        if (member_id_cor != None): return "error"
+        else:
+            app.database.execute(text("""
+                                INSERT INTO sys.member (member_id, member_password, score)
+                                VALUES (:member_id, :member_password, :score)
+                                """), request.json).lastrowid
+            return request.json
