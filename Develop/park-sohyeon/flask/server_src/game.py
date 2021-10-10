eca52c0f46f660427d33e45a6e6daddc3efb6e79
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2021-10-11 02:21:58 +0900

    test

--- Develop/park-sohyeon/flask/server_src/game.py
+++ Develop/park-sohyeon/flask/server_src/game.py
@@ -19,34 +19,43 @@ def game_1(app): # 게임 1 실행 코드
                 return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'', 'answer':'', 'score':0, 'score_ox1':2})
 def game_2(app): # 게임 2 실행 코드
         row = []
+        # 일기 불러오기
         row = app.database.execute(text("""
                                 SELECT text, score_ox2 FROM sys.diary 
                                 WHERE member_id= :member_id and created_at= :created_at
                                 """),request.json).fetchall()
-        
+
         if row != []:
                 if row[0][1] == 1: # 게임을 이미 진행한 경우
                         return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'', 'answer':'', 'score':0, 'score_ox2':1})
-                important_words = NLP_game2.NLP2.make_important_word(row[0][0])
-                if important_words == None:
+                
+                important_words = NLP_game2.NLP2.make_important_word(row[0][0]) # 명사 추출
+                
+                if important_words == None: # 명사 추출에 실패한 경우
                         json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'', 'answer':'', 'score':0, 'score_ox2':2} )
+                
                 game_text = {}
                 game_text['game_text'] = important_words
                 SQL = 'SELECT game_text FROM sys.game WHERE game_text in ("'
                 SQL += '","'.join(game_text['game_text'])
                 SQL += '")'
                 row = app.database.execute(SQL).fetchall()
+                print(row)
                 row_string = ""
                 for i in range(len(row)-1):
                         row_string += ''.join(row[i])+' '
-                if row_string != "":
+                # 장소 관련 단어가 있는 경우    
+                if row_string != "": 
                         row_string += ''.join(row[-1])
+                        print(row_string)
                         return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':row_string, 'score':Read_Game_Score(app), 'score_ox2':0} )
-                else:
+                # 장소 관련 단어가 없는 경우
+                else: 
                         important_word = ""
                         for i in range(len(important_words)-1):
                                 important_word += ''.join(important_words[i])+' '
                         important_word += ''.join(important_words[-1])
+                        print("important" + important_word)
                         return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'h', 'answer':99, 'game_text':important_word, 'score':Read_Game_Score(app), 'score_ox2':0})
         else:
                 return json.dumps({'member_id':request.json['member_id'], 'created_at':request.json['created_at'], 'question':'', 'answer':'', 'score':0, 'score_ox2':3} )
